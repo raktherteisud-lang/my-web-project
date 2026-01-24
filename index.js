ed101d197098138096f60a9088f732eab71e55ee
raktherteisud-lang <raktherteisud@gmail.com>
2026-01-24 17:15:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const form = document.getElementById('loginForm');
-    const errorMsg = document.getElementById('errorMsg');
+const errorMsg = document.getElementById('errorMsg');
 
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
 
-      const userData = localStorage.getItem(`user_${username}`);
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value;
 
-        if (!userData) {
+  const userData = localStorage.getItem(`user_${username}`);
+
+  if (!userData) {
     errorMsg.textContent = "Account not found";
     return;
   }
 
-        const parsedUser = JSON.parse(userData);
+  const parsedUser = JSON.parse(userData);
 
-      if (password === parsedUser.password) {
-        localStorage.setItem('currentUser', username);
-        window.location.href = "loggedin.html";
-      } else {
-        errorMsg.textContent = "Invalid username or password.";
-      }
-
-    });
-
-
+  if (password === parsedUser.password) {
+    localStorage.setItem('currentUser', username);
+    window.location.href = "loggedin.html";
+  } else {
+    errorMsg.textContent = "Invalid username or password.";
+  }
+});
